4b4537ab77ccfd760b6704506ef29378e75ff8f0
suyash-04 <suyashadhikari04@gmail.com>
2024-11-27 23:27:49 +0545

    everything updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from typing import List
 HYPEN_E_DOT='-e .'
-'''def get_requiremet(file_path:str)->List[str]:
+def get_requiremet(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as f:
         requirements=f.readlines()
@@ -9,14 +9,14 @@ HYPEN_E_DOT='-e .'
         
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-    return requirements'''
+    return requirements
    
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 __version__ = "0.0.4"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "MongoDB-Connect"
+PKG_NAME= "MongoConnectPkg"
 AUTHOR_USER_NAME = "suyash-04"
 AUTHOR_EMAIL = "suyashadhikari04@gmail.com"
 setup(
@@ -33,6 +33,6 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    
+    install_requires = get_requiremet("./requirements_dev.txt"),
     
 )
