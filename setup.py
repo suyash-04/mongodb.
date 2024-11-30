d8c74562ccede11a9d6adcdd21c75c3fdaa1e6cc
suyash-04 <suyashadhikari04@gmail.com>
2024-11-30 17:17:25 +0545

    code updated

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 __version__ = "0.0.4"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "MongoConnectPkg"
+PKG_NAME= "MongoConnPKG"
 AUTHOR_USER_NAME = "suyash-04"
 AUTHOR_EMAIL = "suyashadhikari04@gmail.com"
 setup(
