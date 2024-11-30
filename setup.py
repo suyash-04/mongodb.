82d0af5c8d35b520997230470a36faf2396e9ebd
suyash-04 <suyashadhikari04@gmail.com>
2024-11-30 18:29:09 +0545

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
 from typing import List
-
+HYPEN_E_DOT='-e .'
+def get_requiremet(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
@@ -25,6 +34,7 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires = get_requiremet("./requirements_dev.txt")
     )
 
 
