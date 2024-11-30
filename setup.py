4cf5d89e834d357d882b19c177dd1ebc8b70e3d2
suyash-04 <suyashadhikari04@gmail.com>
2024-11-30 18:37:14 +0545

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 from setuptools import setup, find_packages
 from typing import List
-HYPEN_E_DOT='-e .'
-def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
+  
    
 
 __version__ = "0.0.4"
@@ -33,7 +22,14 @@ setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src")
+    packages=find_packages(where="src"),
+    install_requires = ["pymongo",
+"pymongo[srv]",
+"dnspython",
+"pandas",
+"numpy",
+"ensure",
+"pytest"]
     )
 
 
